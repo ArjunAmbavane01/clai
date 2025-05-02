51949b788638bcc165343b63f1fdf3b72747d62f
Arjun <arjun.ambavane@gmail.com>
2025-05-02 18:49:17 +0530

    Fix: remove dist folder

--- dist/components/InputField.js
+++
@@ -1,6 +0,0 @@
-import TextInput from "ink-text-input";
-import React from 'react';
-const InputField = ({ value, onChange, onSubmit, isDisabled }) => {
-    return (React.createElement(TextInput, { value: value, onChange: onChange, onSubmit: onSubmit, placeholder: 'Enter your prompt...', focus: !isDisabled, highlightPastedText: true }));
-};
-export default InputField;
